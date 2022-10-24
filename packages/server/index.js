847128bcea01dcc24f8824c869d0b54407382553
Serg0501 <gavrilenko87sd@gmail.com>
2022-10-24 21:13:09 +0300

    client/server

---
+++ packages/server/index.js
@@ -0,0 +1,12 @@
+//console.log('hello, server is working!!!!');
+
+const http = require('http');
+const PORT = process.env.PORT || 5000;
+const HOST = process.env.HOST || '127.0.0.1';
+const express = require('express');
+
+const app = express();
+const server = http.createServer(app);
+
+server.listen(HOST, PORT, 
+    () => console.log('Server is listening...'));
